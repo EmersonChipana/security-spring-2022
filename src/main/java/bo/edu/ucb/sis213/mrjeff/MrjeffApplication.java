36ed932b3886873b68d9f4269d65feac1c3c2b19
Ernesto Campohermoso <ecampohermoso@cirrus-it.net>
2022-10-20 20:08:16 -0400

    Proyecto base

---
+++ src/main/java/bo/edu/ucb/sis213/mrjeff/MrjeffApplication.java
@@ -0,0 +1,13 @@
+package bo.edu.ucb.sis213.mrjeff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MrjeffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MrjeffApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bo/edu/ucb/sis213/mrjeff/MrjeffApplicationTests.java
@@ -0,0 +1,13 @@
+package bo.edu.ucb.sis213.mrjeff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MrjeffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
